d885bd4b7dca8f99a51614aa0f774edf1bcdd155
nikneym <halildrk@gmail.com>
2025-04-10 22:41:47 +0300

    feat: server instance rewrite

--- src/core/HttpCtx.zig
+++ src/core/HttpCtx.zig
@@ -5,34 +5,15 @@ const std = @import("std");
 const mem = std.mem;
 const assert = std.debug.assert;
 const io = @import("../io.zig");
-const LinkedBuffer = @import("../io/LinkedBuffer.zig");
 
 const HttpCtx = @This();
 /// Owner of this context, I/O operations are done on this.
 socket: io.Socket,
 /// Pointer to event loop to execute I/O operations.
 loop: *io.Loop,
-/// Various flags of the context.
-flags: packed struct {
-    /// Whether or not `Connection: keep-alive` is set.
-    /// We mostly expect requests with keep-alive in HTTP/1.1.
-    persistent: bool = false,
-    /// Whether or not `Transfer-Encoding: chunked` is set.-
-    chunked: bool = false,
-    /// Whether or not `recv_c` is queued for read operations.
-    receiving: bool = false,
-    /// Indicates if the user want to upgrade the protocol.
-    upgrade: enum(u2) { none, http2, websocket, __rsv } = .none,
-    /// Reserved for future usage.
-    /// We try as much as we can to fit flags in a single byte.
-    __rsv: u2 = 0,
-} = .{},
-/// HTTP status to send.
-/// Initially set to `200 OK`.
-status: std.http.Status = .ok,
 /// Completion used for receiving data for this context.
-/// We don't queue multiple receive operations currently since it'd make the code more complex.
-recv_c: io.Completion = .{},
+/// We don't queue multiple receive operations but rather rely on this here.
+read_c: io.Completion = .{},
 /// Allocator owned by this context.
 arena: std.heap.ArenaAllocator,
 /// At the end of this struct, we store a receive buffer that's used for receiving HTTP requests.
@@ -43,22 +24,6 @@ buffer_len: u32 = 0,
 // Buffer is stored here implicitly, call `.buffer()` to get it.
 // buffer: [*]u8,
 
-/// If this is set, caller cannot send `Content-Length` header.
-/// If this is not set, caller cannot send `Transfer-Encoding` header.
-pub inline fn isChunked(ctx: *const HttpCtx) bool {
-    return ctx.flags.chunked;
-}
-
-/// Returns true if `Connection: keep-alive` is set.
-pub inline fn isKeepAlive(ctx: *const HttpCtx) bool {
-    return ctx.flags.persistent;
-}
-
-/// Returns true if the context is currently writing data to it's socket.
-pub inline fn isFlushing(ctx: *const HttpCtx) bool {
-    return ctx.flags.flushing;
-}
-
 /// Internal function.
 /// Returns a pointer to start of trailing buffer.
 pub inline fn bufferPtr(ctx: *HttpCtx) [*]align(@alignOf(HttpCtx)) u8 {
@@ -67,7 +32,7 @@ pub inline fn bufferPtr(ctx: *HttpCtx) [*]align(@alignOf(HttpCtx)) u8 {
 }
 
 /// Internal function.
-/// Returns the read buffer owned by this context.
+/// Returns the whole read buffer owned by this context.
 pub inline fn buffer(ctx: *HttpCtx) []u8 {
     return ctx.bufferPtr()[0..ctx.buffer_cap];
 }
@@ -86,7 +51,7 @@ pub inline fn advance(ctx: *HttpCtx, by: u32) void {
 }
 
 /// Internal function.
-/// Queues the `recv_c` completion for recv operation.
+/// Queues the `recv_c` for recv operation.
 pub fn read(
     ctx: *HttpCtx,
     buf: []u8,
@@ -100,33 +65,24 @@ pub fn read(
         read_len: usize,
     ) void,
 ) void {
-    // Make sure we don't queue the `recv_c` while it's already used.
-    // If this assertion fails, there's definitely a bug.
-    assert(ctx.flags.receiving == false);
-
-    ctx.loop.recv(&ctx.recv_c, HttpCtx, ctx, ctx.socket, buf, comptime struct {
+    return ctx.loop.recv(&ctx.read_c, HttpCtx, ctx, ctx.socket, buf, 0, comptime struct {
         /// Idea here is separating I/O related errors from protocol related errors.
         /// Caller should only focus on errors related to protocol implementation.
         fn intercept(
             _ctx: *HttpCtx,
             _: *io.Loop,
             _: *io.Completion,
-            _: io.Socket,
             _buffer: []u8,
             result: io.RecvError!usize,
         ) void {
-            // `recv_c` is free to use.
-            _ctx.flags.receiving = false;
-
             const len = result catch |err| switch (err) {
-                error.EndOfStream => unreachable, // FIXME: deprecated: we should allow 0 length reads
                 error.WouldBlock => unreachable, // MUST be handled by io package
                 error.SystemResources => @panic("NYI"), // FIXME: NYI
                 error.ConnectionRefused, error.ConnectionResetByPeer, error.ConnectionTimedOut => {
                     // FIXME: NYI
                     @panic("NYI");
                 },
-                error.SocketNotConnected => unreachable, // `loop.recv` must be called after the socket connected
+                error.SocketNotConnected => unreachable, // must be called after the socket connected
                 error.OperationNotSupported => unreachable, // backing protocol must support recv
                 error.PermissionDenied => @panic("EPERM"),
                 error.Cancelled => {
@@ -140,110 +96,4 @@ pub fn read(
             @call(.always_inline, on_read, .{ _ctx, _buffer, len });
         }
     }.intercept);
-
-    ctx.flags.receiving = true;
-}
-
-pub const WriteError = mem.Allocator.Error;
-pub const Writer = std.io.GenericWriter(*HttpCtx, WriteError, write);
-/// Capacity of LinkedBuffer can be as low as LOW_WATERMARK.
-pub const LOW_WATERMARK = 4096;
-
-/// FIXME: Currently this function can write as much as max. u32.
-/// Internal function.
-/// Prefer `Writer` interface via `.writer()` instead.
-///
-/// Creates `Write` objects for given bytes and queues them.
-/// Returns how many bytes written.
-pub fn write(ctx: *HttpCtx, bytes: []const u8) WriteError!usize {
-    // Total number of written bytes from `bytes` so far.
-    var written: usize = 0;
-
-    // If we already have a buffer in tail, we can use it.
-    if (ctx.send_q.tail) |buf| {
-        const len = buf.pushBytes(bytes);
-        // If we have written everything, we're done here.
-        if (len == bytes.len) {
-            return len;
-        }
-
-        // Advance write count.
-        written += len;
-    }
-
-    const remaining = bytes.len - written;
-
-    // Create a new buffer to write remaining bytes.
-    const buf = try LinkedBuffer.create(ctx.arena.allocator(), @max(LOW_WATERMARK, remaining));
-    const len = buf.pushBytes(bytes[written..]);
-    written += len;
-
-    // Queue up.
-    ctx.send_q.push(buf);
-    return written;
-}
-
-/// Writer interface for this context.
-/// This allows writing directly, other writer utilities such as `writeStatus` should be preferred over it.
-pub fn writer(ctx: *HttpCtx) Writer {
-    return Writer{ .context = ctx };
-}
-
-/// Internal function.
-/// Flushes buffers queued in send queue.
-pub fn flushSendQueue(
-    ctx: *HttpCtx,
-    /// Invoked when everything in the send queue has been flushed.
-    /// If the send queue is filled after this invoked, another call to flush is needed.
-    comptime on_flush_end: *const fn (ctx: *HttpCtx) void,
-) void {
-    // If we're already flushing, do nothing.
-    if (ctx.isFlushing()) {
-        return;
-    }
-
-    // Check if we have any buffers to send.
-    if (ctx.send_q.pop()) |node| {
-        // Get the slice we want to send.
-        const buf = node.buffer()[0..node.buffer_len];
-
-        // Queue a send operation.
-        ctx.loop.send(.unlinked, &ctx.send_c, HttpCtx, ctx, ctx.socket, buf, comptime struct {
-            fn interceptor(
-                _ctx: *HttpCtx,
-                _: *io.Loop,
-                _: *io.Completion,
-                _: io.Socket,
-                _buf: []const u8,
-            ) void {
-                // TODO: error handling.
-                // Flushing is finished.
-                _ctx.flags.flushing = false;
-                _ = _buf;
-
-                // Invoke the callback.
-                on_flush_end(_ctx);
-            }
-        }.interceptor);
-
-        // Flushing is started.
-        ctx.flags.flushing = true;
-    }
-}
-
-pub fn flush(ctx: *HttpCtx) void {
-    ctx.flushSendQueue(onFlushEnd);
-}
-
-fn onFlushEnd(ctx: *HttpCtx) void {
-    _ = ctx;
-    std.debug.print("everything sent!\n", .{});
-}
-
-pub inline fn setStatus(ctx: *HttpCtx, status: std.http.Status) void {
-    ctx.status = status;
-}
-
-pub inline fn setHeader(ctx: *HttpCtx, key: []const u8, value: []const u8) mem.Allocator.Error!void {
-    return ctx.headers.put(ctx.arena.allocator(), key, value);
 }
--- src/core/server.zig
+++ src/core/server.zig
@@ -1,152 +1,94 @@
-//! Single HTTP server instance.
-
 const std = @import("std");
 const mem = std.mem;
 const posix = std.posix;
 const io = @import("../io.zig");
-const ContextPool = @import("ctx_pool.zig").ContextPool;
 const HttpCtx = @import("HttpCtx.zig");
 const Request = @import("Request.zig");
 
-const allocator = std.heap.page_allocator;
+const log = std.log.scoped(.@"http-worker");
+
+/// Various options for an instance.
+pub const Tuning = struct {
+    /// Total size of memory owned by a single server instance.
+    /// Instances allocate their memory when they're initialized and never ask system for more.
+    /// Default memory is 16Mb.
+    memory_size: usize = 1024 * 1024 * 16,
+};
 
 /// Passed to each server instance.
-const Handlers = struct {
+pub const Handlers = struct {
     /// Invoked whenever a server receives a request.
     on_request: *const fn (ctx: *HttpCtx) anyerror!void,
     // In the future, we will have a handler for websockets.
-    // In fact, we can add an handler for upgrade requests too.
-    // on_ws: *const fn (ctx: *HttpCtx, wsc: *WebSocketCtx) void,
+    // In fact we can add an handler for upgrade requests too.
 };
 
-/// Intentionally generic, this way we can take comptime arguments in the future.
-pub fn Server(comptime UserType: type, comptime handlers: Handlers) type {
-    _ = UserType;
+const Arena = std.heap.ArenaAllocator;
+const Fba = std.heap.FixedBufferAllocator;
+
+/// Instances are simply decoupled runners of our server.
+/// Each instance owns it's own event loop, memory and listener socket.
+pub fn InstanceImpl(comptime tuning: Tuning, comptime handlers: Handlers) type {
+    _ = handlers;
 
     return struct {
-        const Self = @This();
-        /// Listener socket.
-        fd: io.Socket = io.invalid_socket,
-        /// Whether or not fd is accepting connections at the moment.
-        accepting: bool = false,
-        /// Used by accept operations.
-        accept_c: io.Completion = .{},
-        /// Address of the server.
-        addr: std.net.Address,
+        const Instance = @This();
         /// Event loop owned by this instance.
         loop: io.Loop,
+        /// Listener socket owned by this instance.
+        /// We never share listener sockets; each thread do their own listening thanks to SO_REUSEPORT.
+        listener: io.Socket = io.invalid_socket,
+        /// Used by accept operations.
+        accept_c: io.Completion = .{},
+        /// Active count of connections.
+        conns: usize = 0,
+        /// Heap memory owned by this instance.
+        fba: Fba,
 
-        /// Initializes a server.
-        pub fn init(addr: std.net.Address) !Self {
-            var loop = try io.Loop.init();
-            errdefer loop.deinit();
-
-            // On io_uring, we create direct descriptors table and register the server fd to ring.
-            if (comptime io.backing == .io_uring) {
-                // Create as much file descriptor as possible in the ring.
-                // NOTE: We can increase the file descriptor count here too, via `setrlimit`.
-                const rlimit = try posix.getrlimit(.NOFILE);
-
-                const fd_table: u32 = @intCast(rlimit.max & std.math.maxInt(u32));
-                try loop.directDescriptors(.sparse, fd_table);
-            }
-
-            return .{ .loop = loop, .addr = addr };
+        /// Initializes a server instance.
+        pub fn init(allocator: mem.Allocator) !Instance {
+            const memory = try allocator.alloc(u8, tuning.memory_size);
+            return .{ .loop = try io.Loop.init(), .fba = .init(memory) };
         }
 
-        /// Deinitializes a server.
-        pub fn deinit(server: *Self) void {
-            server.loop.deinit();
+        /// Returns a pointer to instance from pointer to loop.
+        inline fn fromLoop(loop: *io.Loop) *Instance {
+            return @fieldParentPtr("loop", loop);
         }
 
-        /// Creates a file descriptor for accepting connections.
-        fn createServerDescriptor(server: *Self, kernel_backlog: u31) !void {
-            // On io_uring, register the server fd to ring.
-            if (comptime io.backing == .io_uring) {
-                // Create the listener fd.
-                const fd = try server.loop.socket(posix.AF.INET, posix.SOCK.STREAM, posix.IPPROTO.TCP);
-                errdefer posix.close(fd);
-                // Allow binding again.
-                try server.loop.setReuseAddr(fd);
-                // Kernel level load balancing.
-                try server.loop.setReusePort(fd);
-                // Disable nagle's algorithm.
-                try server.loop.setTcpNoDelay(fd, true);
-
-                // Bind to address.
-                try posix.bind(fd, &server.addr.any, server.addr.getOsSockLen());
-                // Listen for connections.
-                try posix.listen(fd, kernel_backlog);
-
-                // NOTE: This part is important since it assigns the server fd to 0.
-                // We can use fd 0 for server's I/O actions from now on.
-                try server.loop.updateDescriptorsSync(0, &[1]io.Socket{fd});
-
-                // Assign 0 as fd.
-                server.fd = 0;
-                // Free the userspace fd.
-                posix.close(fd);
-            } else {
-                @compileError("createServerDescriptor is not implemented for this backend yet");
-            }
-        }
-
-        /// Creates an arena allocator and `HttpCtx` for a given socket.
-        fn createContext(server: *Self, socket: io.Socket) mem.Allocator.Error!*HttpCtx {
-            var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+        /// Intrusively allocates an `HttpCtx` for a given socket.
+        fn createContext(instance: *Instance, socket: io.Socket) mem.Allocator.Error!*HttpCtx {
+            var arena = std.heap.ArenaAllocator.init(instance.fba.allocator());
             errdefer arena.deinit();
 
-            const bytes = try arena.allocator().alignedAlloc(u8, @alignOf(HttpCtx), 4096);
+            const bytes = try arena.allocator().alignedAlloc(u8, @alignOf(HttpCtx), 4096 + @sizeOf(HttpCtx));
             // Interpret first @sizeOf(HttpCtx) bytes as HttpCtx.
             const ctx: *HttpCtx = @ptrCast(bytes.ptr);
 
             // Initialize the context.
             ctx.* = .{
-                .loop = &server.loop,
+                .loop = &instance.loop,
                 .socket = socket,
-                // The context holds it's arena intrusively.
                 .arena = arena,
-                .buffer_cap = 4096 - @sizeOf(HttpCtx),
+                .buffer_cap = 4096,
             };
 
             return ctx;
         }
 
-        /// Returns a pointer to server from pointer to loop.
-        inline fn fromLoop(loop: *io.Loop) *Self {
-            return @fieldParentPtr("loop", loop);
-        }
-
-        /// Run the server.
-        pub fn run(server: *Self) !void {
-            // Setup the server file descriptor.
-            try server.createServerDescriptor(128);
-            // Start accepting connections.
-            server.accept(onClient);
-            // Run the event loop.
-            try server.loop.run();
-        }
-
         /// Internal function.
-        /// Queues the `accept_c` completion for accept operation.
-        /// This can only be stopped via error or `cancelAccept`.
-        /// This function also creates the `HttpCtx` required for the socket
-        /// where caller owns the context.
-        /// SAFETY: If already accepting, this function does nothing and returns.
-        fn accept(server: *Self, comptime on_accept: *const fn (server: *Self, ctx: *HttpCtx) void) void {
-            // If we're already accepting connections, do nothing.
-            if (server.accepting) {
-                return;
-            }
-
-            server.loop.accept(&server.accept_c, Self, server, server.fd, comptime struct {
-                // Interceptor here handles the errors for us.
-                // Not the protocol errors though, they should be handled in `on_accept`.
+        /// Queues the `accept_c` for accept operation. This can only be stopped via error or `cancelAccept`.
+        /// This function also creates the `HttpCtx` required for the socket where caller owns the context.
+        /// # SAFETY:
+        /// If already accepting, this function does nothing and returns.
+        inline fn accept(instance: *Instance, comptime on_accept: *const fn (instance: *Instance, ctx: *HttpCtx) void) void {
+            return instance.loop.accept(&instance.accept_c, Instance, instance, instance.listener, comptime struct {
+                // Interceptor here handles the IO-level errors for us.
+                // Not protocol errors though, they should be handled in `on_accept`.
                 fn intercept(
-                    _server: *Self,
+                    _inst: *Instance,
                     _: *io.Loop,
                     _: *io.Completion,
-                    _: io.Socket,
                     result: io.AcceptError!io.Socket,
                 ) void {
                     const socket = result catch |err| switch (err) {
@@ -155,69 +97,93 @@ pub fn Server(comptime UserType: type, comptime handlers: Handlers) type {
                         error.Cancelled => return, // operation cancelled
                         error.SocketNotListening => unreachable, // we're definitely listening
                         error.ProcessFdQuotaExceeded, error.SystemFdQuotaExceeded, error.SystemResources => {
-                            // Complete serving connections that're currently processing since we don't have any slot left.
+                            // TODO: Complete serving connections that're currently processing since we don't have any slot left.
                             // We can continue accepting after an fd is closed.
-                            _server.accepting = false;
-                            return;
+                            @panic("OOM");
                         },
-                        error.OperationNotSupported => unreachable, // we're on TCP
-                        error.PermissionDenied => @panic("EPERM"),
+                        error.OperationNotSupported => unreachable, // underlying protocol must support accept operations
+                        error.PermissionDenied => unreachable,
                         error.ProtocolFailure => unreachable,
                         error.Unexpected => unreachable,
                     };
 
                     // Create a context for the socket.
-                    const ctx = _server.createContext(socket) catch |err| switch (err) {
+                    const ctx = _inst.createContext(socket) catch |err| switch (err) {
                         error.OutOfMemory => {
                             // We can't handle connections at the moment.
                             @panic("OOM");
                         },
                     };
 
-                    @call(.always_inline, on_accept, .{ _server, ctx });
+                    @call(.auto, on_accept, .{ _inst, ctx });
                 }
             }.intercept);
-
-            // We queued an accept operation.
-            server.accepting = true;
         }
 
-        /// Cancels accepting connections.
-        /// TODO: impl
-        fn cancelAccept(_: *Self) void {
-            @panic("Not implemented yet");
+        fn onAccept(instance: *Instance, ctx: *HttpCtx) void {
+            // Increase HTTP connection count.
+            instance.conns += 1;
+
+            // HTTP transaction starts.
+            // Read the next request.
+            ctx.read(ctx.buffer(), onRequestInitRead);
         }
 
-        /// Emitted whenever a new client drops by.
-        fn onClient(_: *Self, ctx: *HttpCtx) void {
-            // We start the HTTP cycle.
-            ctx.read(ctx.buffer(), onRead);
-        }
+        /// # SAFETY:
+        /// 0 reads are not interpretted as errors; should be handled here.
+        fn onRequestInitRead(ctx: *HttpCtx, _: []u8, read_len: usize) void {
+            // Check if other side is closed.
+            if (read_len == 0) {
+                @panic("peer is closed");
+            }
 
-        /// Internal function.
-        /// Emitted when there's HTTP request data, this will try to parse the HTTP request out of it.
-        fn onRead(ctx: *HttpCtx, _: []u8, read_len: usize) void {
-            // Advance as much as we've received.
-            ctx.advance(@intCast(read_len));
+            // Increment the buffer length as much as read.
+            ctx.buffer_len += @intCast(read_len);
 
-            // Try to parse the HTTP request.
+            const whole = ctx.bufferPtr()[0..ctx.buffer_len];
+            // Try to parse an HTTP request.
             var req = Request{};
-            // `consumed` returns how many bytes request line (GET / HTTP/1.1) + headers consist of.
-            const consumed = req.parse(ctx.buffer()[0..ctx.buffer_len]) catch |err| switch (err) {
-                // Read more.
-                error.Incomplete => return ctx.read(ctx.buffer(), onRead),
-                error.Invalid => @panic("invalid request"), // FIXME: NYI
-            };
-            _ = consumed;
+            const req_len = req.parse(whole) catch |err| switch (err) {
+                error.Incomplete => {
+                    const remaining = ctx.bufferAvailable();
+                    // Request is larger than we can handle; we reject accepting requests > 4096 bytes.
+                    if (remaining.len == 0) {
+                        @panic("this should be handled as invalid too");
+                    }
 
-            // If we got here, request is parsed successfully.
-            // We can invoke the user-provided `on_request`.
-            handlers.on_request(ctx) catch {
-                // FIXME: handle error
-                @panic("NYI");
+                    return ctx.read(remaining, onRequestInitRead);
+                },
+                error.Invalid => @panic("handle invalid request"),
             };
+            _ = req_len;
 
-            ctx.flush();
+            log.info("(thread-{}) {s} {?s}", .{ std.Thread.getCurrentId(), @tagName(req.method), req.path });
+
+            // Check if we can keep alive this request.
+            std.debug.print("{}\n", .{req.isKeepAlive()});
+        }
+
+        /// Starts serving connections.
+        pub fn run(instance: *Instance, port: u16) !void {
+            // Create our listener socket.
+            const listener = try posix.socket(posix.AF.INET, posix.SOCK.STREAM, posix.IPPROTO.TCP);
+            errdefer posix.close(listener);
+
+            const addr = std.net.Address.initIp4(.{ 127, 0, 0, 1 }, port);
+            try instance.loop.setReuseAddr(listener);
+            try instance.loop.setReusePort(listener);
+            try instance.loop.setTcpNoDelay(listener, true);
+            try posix.bind(listener, &addr.any, addr.getOsSockLen());
+            try posix.listen(listener, 128);
+
+            instance.listener = listener;
+
+            // Queue the accept_c.
+            instance.accept(onAccept);
+
+            log.info("(thread-{}) listening for connections", .{std.Thread.getCurrentId()});
+            // Run event loop.
+            return instance.loop.run();
         }
     };
 }
